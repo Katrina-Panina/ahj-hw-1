bf11464e9797d6a56bc762484db2bd758cef5309
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-08-21 19:12:33 +0300

    fix

--- src/js/__tests__/app.js
+++ src/js/__tests__/app.js
@@ -1,6 +1,6 @@
-import demo from "../app";
+import demo from '../app';
 
-test("The demo function return value", () => {
-  const receivedValue = "test";
-  expect(demo("test")).toBe(receivedValue);
+test('The demo function return value', () => {
+  const receivedValue = 'test';
+  expect(demo('test')).toBe(receivedValue);
 });
